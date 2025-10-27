80a4590b19c1bc66633dfd1d04825fb11c66700f
spoooktune <asriprasana5@gmail.com>
2025-10-26 21:52:55 -0700

    Formatted display, added races for reading

--- main.cpp
+++ main.cpp
@@ -5,28 +5,85 @@
 #include <set>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <iomanip>
+
 using namespace std;
 using namespace std::chrono;
 
 int main() {
-    auto start = high_resolution_clock::now();
-    vector <string> codes;
+    const int W = 10;
     ifstream in;
-    in.open("codes.txt");
     string x;
+
+    cout << setw(W) << "Operation" <<
+        setw(W) << "Vector" <<
+        setw(W) << "List" << 
+        setw(W) << "Set" << endl;
+    
+    cout << setw(W) << "Read";
+
+    // Vector read
+    auto start = high_resolution_clock::now();
+    vector<string> codesV;
+    in.open("codes.txt");
     if (in.good()){
         while (!in.eof()){
             getline(in, x);
-            codes.push_back(x);
+            codesV.push_back(x);
         }
     }
     else{
         cout << "unable to open file";
     }
+    in.close();
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start);
-    cout << duration.count();
+    cout << setw(W) << duration.count();
 
+    // List read
+    start = high_resolution_clock::now();
+    list<string> codesL;
+    in.open("codes.txt");
+    if (in.good()){
+        while (!in.eof()){
+            getline(in, x);
+            codesL.push_back(x);
+        }
+    }
+    else{
+        cout << "unable to open file";
+    }
+    in.close();
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << setw(W) << duration.count();
+
+    // Set read
+    start = high_resolution_clock::now();
+    set<string> codesS;
+    in.open("codes.txt");
+    if (in.good()){
+        while (!in.eof()){
+            getline(in, x);
+            codesS.insert(x);
+        }
+    }
+    else{
+        cout << "unable to open file";
+    }
+    in.close();
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << setw(W) << duration.count() << endl;
+
+    cout << setw(W) << "Sort";
+    // Vector sort
+    start = high_resolution_clock::now();
+    sort(codesV.begin(), codesV.end());
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << setw(W) << duration.count();
     return 0;
 }
 
